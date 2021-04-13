c0817d27f783c5cca7796ec08a3e7c5e5e8947f2
larryfisherman <szczepannowak87@gmail.com>
2021-04-13 14:48:15 +0200

    Implemented second subpage - checkout, shopping basket

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,18 @@
 import React from "react";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
-      <img
-        className="header__logo"
-        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-        alt="amazonLogo"
-      />
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt="amazonLogo"
+        />
+      </Link>
       <div className="header__search">
         <input type="text" className="header__searchInput" />
         <SearchIcon className="header__searchIcon" />
@@ -27,12 +30,14 @@ function Header() {
           <span className="header__navOption__lineOne">Your</span>
           <span className="header__navOption__lineTwo">Prime</span>
         </div>
-        <div className="header__basketOption">
-          <ShoppingBasketIcon />
-          <span className="header__navOption__lineTwo header__basketCount">
-            0
-          </span>
-        </div>
+        <Link to="/checkout">
+          <div className="header__basketOption">
+            <ShoppingBasketIcon />
+            <span className="header__navOption__lineTwo header__basketCount">
+              0
+            </span>
+          </div>
+        </Link>
       </div>
     </div>
   );
