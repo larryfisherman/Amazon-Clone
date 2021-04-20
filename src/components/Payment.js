df392f687dd6fd862c14554e9e5681a8cef18729
larryfisherman <szczepannowak87@gmail.com>
2021-04-20 14:49:42 +0200

    Fixed bug with client_secret

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -57,6 +57,7 @@ function Payment() {
             amount: paymentIntent.amount,
             created: paymentIntent.created,
           });
+
         setSucceeded(true);
         setError(null);
         setProcessing(false);
